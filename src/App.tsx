3cabcb4eb3eeba2b77472863d57e1bde9b4c316f
Arthur Amaral <arthuramaral6@gmail.com>
2023-12-15 22:34:36 -0300

    fetching data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { SideBar } from './components/SideBar/SideBar'
+
+import axios from 'axios'
 
 export function App() {
   const [sideBarOpen, setSideBarOpen] = useState(true)
 
+  const [apiData, setApiData] = useState([])
+
+  const apiUrl = 'https://api.github.com/users'
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   function handleSideBar() {
     setSideBarOpen(!sideBarOpen)
+  }
+
+  function fetchData() {
+    axios
+      .get(apiUrl)
+      .then(response => {
+        setApiData(response.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
   }
 
   return (
