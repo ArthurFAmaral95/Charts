761bac85e290c7b62f6949b4e14384f077cf8ee6
Arthur Amaral <arthuramaral6@gmail.com>
2024-01-03 19:40:42 -0300

    UserForm layout

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,3 +1,5 @@
+import './styles.css'
+
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -23,12 +25,19 @@ export function UserForm(props: HandleFormSubmit) {
 
   return (
     <form id="user-form" onSubmit={handleSubmit(sendLogin)}>
-      <div className="input-text">
+      <div className="form-labels">
         <label htmlFor="login">User Login</label>
         {errors.login && <span>{errors.login.message}</span>}
       </div>
-      <input type="text" id="login" {...register('login')} />
-      <button>Search</button>
+      <div className="form-input">
+        <input
+          type="text"
+          id="login"
+          {...register('login')}
+          placeholder="Search for an user here"
+        />
+        <button></button>
+      </div>
     </form>
   )
 }
